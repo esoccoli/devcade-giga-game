8644a973355a7a16db9b69b436da4316badff100
matthewlefler <matthewlefler1@gmail.com>
2025-01-29 16:00:12 -0500

    added testing manager script and test goal object

--- world-of-wallhoppers/assets/scripts/camera_lerp_to_position.gd
+++ world-of-wallhoppers/assets/scripts/camera_lerp_to_position.gd
@@ -1,24 +1,21 @@
 extends Camera2D
 
+var players 
+
 @export var object: Node2D
 
-@export var interpolation_speed: float = 3
+@export var interpolation_speed: float = 3.0
 
-@export var interpolation_cutoff: float = 100 # in pixels
+@export var interpolation_cutoff: float = 10.0 # in pixels
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = basic_lerp(position, object.position, delta)
 
 
 ## the function below is possibly inferior
 ## but it does do some nice looking vibrations at higher object velocities
-
 func basic_lerp(from: Vector2, to: Vector2, delta: float) -> Vector2:
 	var relative_offset = from - to
 
